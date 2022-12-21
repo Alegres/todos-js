aa10039654e9d5bc270a80cec74ce32d373b32fa
Damian Cywinski <damian.cywinski@capgemini.com>
2022-12-21 11:38:48 +0100

    Clean up of project structure

--- src/todos/TodosView.js
+++ src/todos/TodosView.js
@@ -1,46 +1,40 @@
-import {AbstractView} from "../view/AbstractView";
+import { AbstractView } from "../view/AbstractView";
 import { getTodos } from "../todos/todosService";
 
 
 export class TodosView extends AbstractView {
-    constructor() {
-       super();
-    }
+  constructor() {
+    super();
+  }
 
-
-  async  loadModel(){
-     console.log("Loading...");  
+  async loadModel() {
+    console.log("Loading...");
     this.todos = await getTodos();
-        console.log(this.todos);
-        
-    }
+    console.log(this.todos);
 
+  }
 
-     getHTML() {
-        return `
-
-        <div>
-            <h1>TODO</h1>
-            <button id="click">Get data</button>
-            <div id="todos"></div>
-            </div>
+  getHTML() {
+    return `
+          <div>
+              <h1>TODO</h1>
+              <div id="todos"></div>
+          </div>
         `
-    }
+  }
 
-  
+  renderContent() {
+    let todosListElement = document.querySelector('#todos')
+    todosListElement.innerHTML = ``;
 
-    renderContent() {
-      let todosListElement = document.querySelector('#todos')
-      todosListElement.innerHTML= ``;
+    this.todos.forEach(todo => {
+      let todoElement = document.createElement(`li`);
+      todoElement.innerHTML = todo.title;
+      todosListElement.appendChild(todoElement);
 
-      this.todos.forEach(element => {
-        let todoElement = document.createElement(`li`);
-        todoElement.innerHTML = todo.title;
-        todosListElement.appendChild(todoElement);
-
-        let checkbox = document.createElement(`input`);
-        checkbox.type = 'checkbox';
-        todosListElement.appendChild(checkbox);
-      });
-    }
+      let checkbox = document.createElement(`input`);
+      checkbox.type = 'checkbox';
+      todosListElement.appendChild(checkbox);
+    });
+  }
 }
